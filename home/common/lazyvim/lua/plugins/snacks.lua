2e9dec161888661ea98cb612a6fd6c26a0627076
Lucas <keyruu@web.de>
2025-05-20 22:05:05 +0200

    add proper zen styling

--- home/common/lazyvim/lua/plugins/snacks.lua
+++ home/common/lazyvim/lua/plugins/snacks.lua
@@ -1,5 +1,5 @@
 return {
-  "snacks.nvim",
+  "folke/snacks.nvim",
   keys = {
     { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
     { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
@@ -13,6 +13,14 @@ return {
         require("snacks.terminal").toggle("fish")
       end,
       desc = "[T]erminal [f]loating",
+    },
+  },
+  opts = {
+    ---@type table<string, snacks.win.Config>
+    styles = {
+      zen = {
+        backdrop = { transparent = false, bg = "#0c0e0f" },
+      },
     },
   },
 }
