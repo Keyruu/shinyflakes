25ad15d6545305566fa9aa1429bafb6ba6c4e478
Lucas <keyruu@web.de>
2025-12-08 23:25:41 +0100

    add folders to syncthing

--- nix/modules/nixos/syncthing.nix
+++ nix/modules/nixos/syncthing.nix
@@ -16,59 +16,60 @@
       devices = {
         fairphone.id = "32BRNST-FVHJCGH-JJXLLZD-5B7DXEG-FJHOYKE-YJV7MCY-6I4NOB4-HJYBFA5";
         muadib.id = "2X36LHE-IOQZGKU-G7B53FJ-ILY6TMU-QXQSHLV-C7PIQ4U-JZUVITM-KV37FAB";
+        mentat.id = "RR5NKJX-GZGJ22Z-OC5MA3P-HEAGZK6-QD7CHUK-TNARPTX-H7OCUBD-URI7MAO";
       };
 
-      # folders =
-      #   let
-      #     dir = config.services.syncthing.dataDir;
-      #   in
-      #   {
-      #     git = {
-      #       enable = lib.mkDefault true;
-      #       path = "${dir}/git";
-      #       devices = [
-      #         "mentat"
-      #         "carryall"
-      #         "muadib"
-      #         "thopter"
-      #         "fairphone"
-      #       ];
-      #       ignorePerms = false;
-      #     };
-      #     documents = {
-      #       enable = lib.mkDefault true;
-      #       path = "${dir}/documents";
-      #       devices = [
-      #         "mentat"
-      #         "carryall"
-      #         "muadib"
-      #         "thopter"
-      #         "fairphone"
-      #       ];
-      #     };
-      #     work = {
-      #       enable = lib.mkDefault true;
-      #       path = "${dir}/work";
-      #       devices = [
-      #         "mentat"
-      #         "carryall"
-      #         "muadib"
-      #         "thopter"
-      #       ];
-      #       ignorePerms = false;
-      #     };
-      #     obsidian = {
-      #       enable = lib.mkDefault true;
-      #       path = "${dir}/obsidian";
-      #       devices = [
-      #         "mentat"
-      #         "carryall"
-      #         "muadib"
-      #         "thopter"
-      #         "fairphone"
-      #       ];
-      #     };
-      #   };
+      folders =
+        let
+          dir = config.services.syncthing.dataDir;
+        in
+        {
+          git = {
+            enable = lib.mkDefault true;
+            path = "${dir}/git";
+            devices = [
+              "mentat"
+              # "thopter"
+              # "carryall"
+              "muadib"
+              "fairphone"
+            ];
+            ignorePerms = false;
+          };
+          documents = {
+            enable = lib.mkDefault true;
+            path = "${dir}/documents";
+            devices = [
+              "mentat"
+              # "thopter"
+              # "carryall"
+              "muadib"
+              "fairphone"
+            ];
+          };
+          work = {
+            enable = lib.mkDefault true;
+            path = "${dir}/work";
+            devices = [
+              "mentat"
+              # "thopter"
+              # "carryall"
+              "muadib"
+            ];
+            ignorePerms = false;
+          };
+          obsidian = {
+            enable = lib.mkDefault true;
+            path = "${dir}/obsidian";
+            devices = [
+              "mentat"
+              # "thopter"
+              # "carryall"
+              "muadib"
+              "fairphone"
+            ];
+          };
+        };
     };
   };
 }
