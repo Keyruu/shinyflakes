115539a691528469291d9147f5b8302f314cddfc
Lucas <keyruu@web.de>
2025-12-26 13:57:03 +0100

    try something

--- nix/hosts/mentat/modules/stacks/arr/sabnzbd.nix
+++ nix/hosts/mentat/modules/stacks/arr/sabnzbd.nix
@@ -34,7 +34,7 @@ in
       };
     };
     torrent-gluetun.containerConfig.publishPorts = [
-      "127.0.0.1:8080:8080"
+      "127.0.0.1:8085:8085"
     ];
   };
 
@@ -44,7 +44,7 @@ in
       forceSSL = true;
 
       locations."/" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://127.0.0.1:8085";
         proxyWebsockets = true;
       };
     };
