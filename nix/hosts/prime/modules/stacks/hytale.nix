62d75d0f7d3a2790ecd189f7fade5ff3634e656a
Lucas <keyruu@web.de>
2026-01-16 16:21:38 +0100

    add podman arg

--- nix/hosts/prime/modules/stacks/hytale.nix
+++ nix/hosts/prime/modules/stacks/hytale.nix
@@ -41,6 +41,9 @@ in
         TZ = "Europe/Amsterdam";
         CACHE = "TRUE";
       };
+      podmanArgs = [
+        "-a=stdin"
+      ];
     };
     serviceConfig = {
       Restart = "always";
