a0d9a1d065290d9b96de4e2308e64fa2b2fd3996
Lucas <keyruu@web.de>
2026-01-19 19:52:42 +0100

    stop hytale server

--- nix/hosts/mentat/modules/stacks/hytale.nix
+++ nix/hosts/mentat/modules/stacks/hytale.nix
@@ -25,34 +25,34 @@ in
     '';
   };
 
-  virtualisation.quadlet.containers.hytale-server = {
-    containerConfig = {
-      image = "docker.io/deinfreu/hytale-server:experimental-0.1.3";
-      publishPorts = [ "5520:5520/udp" ];
-      volumes = [
-        "${stackPath}/data:/home/container"
-        "/etc/machine-id:/etc/machine-id:ro"
-      ];
-      environmentFiles = [ config.sops.templates."hytale-server.env".path ];
-      environments = {
-        SERVER_IP = "0.0.0.0";
-        SERVER_PORT = "5520";
-        PROD = "FALSE";
-        DEBUG = "FALSE";
-        TZ = "Europe/Amsterdam";
-        CACHE = "TRUE";
-        HYTALE_SERVER_NAME = "Der Server";
-        HYTALE_MOTD = "Was geht ab, ihr Oberloser?";
-        HYTALE_MAX_PLAYERS = "1000";
-        HYTALE_MAX_VIEW_RADIUS = "32";
-        HYTALE_COMPRESSION = "false";
-        HYTALE_WORLD = "petershausen";
-        HYTALE_GAMEMODE = "Adventure";
-        HYTALE_ALLOW_OP = "TRUE";
-      };
-    };
-    serviceConfig = {
-      Restart = "always";
-    };
-  };
+  # virtualisation.quadlet.containers.hytale-server = {
+  #   containerConfig = {
+  #     image = "docker.io/deinfreu/hytale-server:experimental-0.1.3";
+  #     publishPorts = [ "5520:5520/udp" ];
+  #     volumes = [
+  #       "${stackPath}/data:/home/container"
+  #       "/etc/machine-id:/etc/machine-id:ro"
+  #     ];
+  #     environmentFiles = [ config.sops.templates."hytale-server.env".path ];
+  #     environments = {
+  #       SERVER_IP = "0.0.0.0";
+  #       SERVER_PORT = "5520";
+  #       PROD = "FALSE";
+  #       DEBUG = "FALSE";
+  #       TZ = "Europe/Amsterdam";
+  #       CACHE = "TRUE";
+  #       HYTALE_SERVER_NAME = "Der Server";
+  #       HYTALE_MOTD = "Was geht ab, ihr Oberloser?";
+  #       HYTALE_MAX_PLAYERS = "1000";
+  #       HYTALE_MAX_VIEW_RADIUS = "32";
+  #       HYTALE_COMPRESSION = "false";
+  #       HYTALE_WORLD = "petershausen";
+  #       HYTALE_GAMEMODE = "Adventure";
+  #       HYTALE_ALLOW_OP = "TRUE";
+  #     };
+  #   };
+  #   serviceConfig = {
+  #     Restart = "always";
+  #   };
+  # };
 }
