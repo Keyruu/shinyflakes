cda70cf4142508da71d7f2cd78c958d038ec0268
Lucas <keyruu@web.de>
2026-02-12 11:53:06 +0100

    fix removed stacks

--- nix/hosts/mentat/modules/stacks/default.nix
+++ nix/hosts/mentat/modules/stacks/default.nix
@@ -6,19 +6,14 @@
     ./search
 
     ./immich.nix
-    # ./ai.nix
     ./traccar.nix
     ./speedtest-tracker.nix
-    # ./wud.nix
     ./librechat
     ./karakeep.nix
     ./timetagger.nix
-    ./pinchflat.nix
-    # ./pigallery2.nix
     ./radicale.nix
     ./isponsorblocktv.nix
     # ./n8n.nix
-    ./garage.nix
     ./karaoke.nix
     ./dawarich.nix
     ./hytale.nix
