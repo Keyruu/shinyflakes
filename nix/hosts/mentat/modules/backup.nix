8f26a076918f9ab4ca38b790fb38d26f82ffe8e3
Lucas <keyruu@web.de>
2026-02-17 09:35:38 +0100

    use rest server for all backups

--- nix/hosts/mentat/modules/backup.nix
+++ nix/hosts/mentat/modules/backup.nix
@@ -1,20 +1,21 @@
 { config, ... }:
-let
-  repo = "/main/backup/restic";
-in
 {
   users.groups.restic = { };
   users.users.restic = {
     isSystemUser = true;
     group = "restic";
   };
-  sops.secrets.resticHtpasswd = {
-    owner = "restic";
-    group = "restic";
+  sops = {
+    secrets.resticHtpasswd = {
+      owner = "restic";
+      group = "restic";
+    };
+    templates."resticRepo".content =
+      "rest:http://lucas:${config.sops.placeholder.resticServerPassword}@127.0.0.1:8004/restic";
   };
 
   services.restic = {
-    defaultRepo = repo;
+    defaultRepoFile = config.sops.templates."resticRepo".path;
     server = {
       enable = true;
       dataDir = "/main/backup";
