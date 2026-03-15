560ac03b236b85a173d275983675a543d06543af
Lucas <keyruu@web.de>
2026-03-15 16:15:13 +0100

    fix: always use a fake hash bc otherwise it pulls from cache

--- nix/packages/update-hash.nix
+++ nix/packages/update-hash.nix
@@ -20,12 +20,10 @@ pkgs.writeShellApplication {
       exit 1
     fi
 
-    output=$(nix build ".#''${pkgName}" 2>&1) && exit_code=0 || exit_code=$?
+    fakeHash="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+    sed -i "s|hash = \"[^\"]*\"|hash = \"$fakeHash\"|" "$file"
 
-    if [ "$exit_code" -eq 0 ]; then
-      echo "Hash is already correct"
-      exit 0
-    fi
+    output=$(nix build ".#''${pkgName}" 2>&1) && exit_code=0 || exit_code=$?
 
     correctHash=$(echo "$output" | grep 'got:' | grep -o 'sha256-[A-Za-z0-9+/=]*')
 
