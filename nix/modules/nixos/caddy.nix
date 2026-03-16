82ec1da181b047614c2fd9cfb752afed9a85f07f
Lucas <keyruu@web.de>
2026-03-16 17:45:01 +0100

    fix: remove log config

--- nix/modules/nixos/caddy.nix
+++ nix/modules/nixos/caddy.nix
@@ -42,15 +42,6 @@
 
       globalConfig = ''
         order coraza_waf first
-        log default {
-          output file /var/log/caddy/access.log {
-            roll_size 100MiB
-            roll_keep 5
-            roll_keep_for 168h
-          }
-          format json
-          level INFO
-        }
         servers {
           trusted_proxies static ${lib.concatStringsSep " " all}
           trusted_proxies_strict
