7f1c0bf0106121b8887998c5216efbbd443748d4
Lucas <keyruu@web.de>
2026-04-04 14:21:47 +0200

    feat: use nixosmodule for scanservejs

--- nix/hosts/mentat/modules/scan.nix
+++ nix/hosts/mentat/modules/scan.nix
@@ -1,25 +1,10 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
 let
   my = config.services.my.scanservjs;
+  scansPath = "${my.stack.path}/scans";
 in
 {
-  hardware.sane = {
-    enable = true;
-    brscan4 = {
-      enable = true;
-    };
-  };
-
-  services.saned = {
-    enable = true;
-    extraConfig = ''
-      10.0.0.0/8
-      172.16.0.0/12
-    '';
-  };
-
-  # Allow the scanservjs container to reach saned on the host
-  networking.firewall.interfaces."podman0".allowedTCPPorts = [ 6566 ];
+  hardware.sane.brscan4.enable = true;
 
   services.my.scanservjs = {
     port = 8070;
@@ -28,64 +13,42 @@ in
       enable = true;
       cert.host = "port.peeraten.net";
     };
+    backup.enable = true;
     stack = {
       enable = true;
       directories = [
         "scans"
         "scans/lucas"
         "scans/nadine"
-        "config"
       ];
-      security.enable = true;
-
-      containers.scanservjs = {
-        containerConfig = {
-          image = "docker.io/sbs20/scanservjs:v3.0.4";
-          publishPorts = [ "127.0.0.1:${toString my.port}:8080" ];
-          environments = {
-            SANED_NET_HOSTS = "host.containers.internal";
-          };
-          addHosts = [ "host.containers.internal:host-gateway" ];
-          volumes = [
-            "${my.stack.path}/scans:/scans"
-            "${my.stack.path}/config:/etc/scanservjs"
-          ];
-        };
-        # scanservjs runs as root and needs to write to data/ and tmp dirs
-        security = {
-          readOnlyRootFilesystem = false;
-          dropAllCapabilities = false;
-        };
-      };
     };
   };
 
-  environment.etc."stacks/scanservjs/config/config.local.js" = {
-    text = # js
+  services.scanservjs = {
+    enable = true;
+    settings = {
+      host = "127.0.0.1";
+      port = my.port;
+    };
+    extraActions = [
       ''
-        const options = { paths: ['/usr/lib/scanservjs'] };
-        const Process = require(require.resolve('./server/classes/process', options));
-
-        module.exports = {
-          actions: [
-            {
-              name: 'Send to Lucas',
-              async execute(fileInfo) {
-                return await Process.spawn("cp '" + fileInfo.fullname + "' /scans/lucas/");
-              }
-            },
-            {
-              name: 'Send to Nadine',
-              async execute(fileInfo) {
-                return await Process.spawn("cp '" + fileInfo.fullname + "' /scans/nadine/");
-              }
-            }
-          ]
-        };
-      '';
+        {
+          name: 'Send to Lucas',
+          async execute(fileInfo) {
+            const Process = require('./server/classes/process');
+            return await Process.spawn("cp '" + fileInfo.fullname + "' '${scansPath}/lucas/'");
+          }
+        }
+      ''
+      ''
+        {
+          name: 'Send to Nadine',
+          async execute(fileInfo) {
+            const Process = require('./server/classes/process');
+            return await Process.spawn("cp '" + fileInfo.fullname + "' '${scansPath}/nadine/'");
+          }
+        }
+      ''
+    ];
   };
-
-  virtualisation.quadlet.containers.scanservjs.unitConfig."X-RestartTrigger" = [
-    config.environment.etc."stacks/scanservjs/config/config.local.js".source
-  ];
 }
