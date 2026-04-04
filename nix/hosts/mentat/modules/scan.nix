010d2929b822d5c5ce1f464517b37c3b3ae2ebaf
Lucas <keyruu@web.de>
2026-04-04 15:59:47 +0200

    fix: try to override config manually

--- nix/hosts/mentat/modules/scan.nix
+++ nix/hosts/mentat/modules/scan.nix
@@ -1,7 +1,14 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   my = config.services.my.scanservjs;
   scansPath = "${my.stack.path}/scans";
+  # The scanservjs package path where config/config.local.js is expected
+  scanservjsPackagePath = "${pkgs.scanservjs}/lib/node_modules/scanservjs";
 in
 {
   hardware.sane.brscan4.enable = true;
@@ -51,4 +58,11 @@ in
       ''
     ];
   };
+
+  # The NixOS module sets NIX_SCANSERVJS_CONFIG_PATH but the package doesn't
+  # read it. scanservjs loads config from ../../config/config.local.js relative
+  # to its source. We bind-mount the config file into the expected location.
+  systemd.services.scanservjs.serviceConfig.BindPaths = [
+    "${config.systemd.services.scanservjs.environment.NIX_SCANSERVJS_CONFIG_PATH}:${scanservjsPackagePath}/config/config.local.js"
+  ];
 }
