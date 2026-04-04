81df4daebc23a97731aa1caaabbe49259be32ee8
Lucas <keyruu@web.de>
2026-04-04 13:30:55 +0200

    fix: adjust scanservjs pipeline

--- nix/hosts/mentat/modules/scan.nix
+++ nix/hosts/mentat/modules/scan.nix
@@ -61,83 +61,70 @@ in
   };
 
   environment.etc."stacks/scanservjs/config/config.local.js" = {
-    text = ''
-      module.exports = {
-        afterConfig(config) {
-          config.pipelines = [
-            {
-              extension: 'pdf',
-              description: 'Lucas - PDF',
-              commands: [
-                'convert @- -quality 92 tmp-%04d.jpg && ls tmp-*.jpg',
-                'convert @- scan-0000.pdf',
-                'mv scan-0000.pdf ../output/lucas/ && ls ../output/lucas/scan-0000.pdf'
-              ]
-            },
-            {
-              extension: 'pdf',
-              description: 'Nadine - PDF',
-              commands: [
-                'convert @- -quality 92 tmp-%04d.jpg && ls tmp-*.jpg',
-                'convert @- scan-0000.pdf',
-                'mv scan-0000.pdf ../output/nadine/ && ls ../output/nadine/scan-0000.pdf'
-              ]
-            },
-            {
-              extension: 'jpg',
-              description: 'Lucas - JPG',
-              commands: [
-                'convert @- -quality 92 scan-%04d.jpg',
-                'ls scan-*.jpg'
-              ],
-              afterAction: 'Move to Lucas'
-            },
-            {
-              extension: 'jpg',
-              description: 'Nadine - JPG',
-              commands: [
-                'convert @- -quality 92 scan-%04d.jpg',
-                'ls scan-*.jpg'
-              ],
-              afterAction: 'Move to Nadine'
-            }
-          ];
-        },
-
-        afterDevices(devices) {
-          // Fix Brother scanner dimensions (they often report incorrectly)
-          devices
-            .filter(d => d.id.includes('brother'))
-            .forEach(device => {
-              device.features['-l'].limits = [0, 215];
-              device.features['-t'].limits = [0, 297];
-              device.features['-x'].default = 215;
-              device.features['-x'].limits = [0, 215];
-              device.features['-y'].default = 297;
-              device.features['-y'].limits = [0, 297];
-            });
-        },
-
-        actions: [
-          {
-            name: 'Move to Lucas',
-            async execute(fileInfo) {
-              const options = { paths: ['/usr/lib/scanservjs'] };
-              const Process = require(require.resolve('./server/classes/process', options));
-              return await Process.spawn("mv '" + fileInfo.fullname + "' /var/lib/scanservjs/output/lucas/");
-            }
+    text = # js
+      ''
+        module.exports = {
+          afterConfig(config) {
+            config.pipelines = [
+              {
+                extension: 'pdf',
+                description: 'Lucas - PDF',
+                commands: [
+                  'convert @- -quality 92 tmp-%04d.jpg && ls tmp-*.jpg',
+                  'convert @- scan-0000.pdf',
+                  'mv scan-0000.pdf ../output/lucas/ && ls ../output/lucas/scan-0000.pdf'
+                ]
+              },
+              {
+                extension: 'pdf',
+                description: 'Nadine - PDF',
+                commands: [
+                  'convert @- -quality 92 tmp-%04d.jpg && ls tmp-*.jpg',
+                  'convert @- scan-0000.pdf',
+                  'mv scan-0000.pdf ../output/nadine/ && ls ../output/nadine/scan-0000.pdf'
+                ]
+              },
+              {
+                extension: 'jpg',
+                description: 'Lucas - JPG',
+                commands: [
+                  'convert @- -quality 92 scan-%04d.jpg',
+                  'ls scan-*.jpg'
+                ],
+                afterAction: 'Move to Lucas'
+              },
+              {
+                extension: 'jpg',
+                description: 'Nadine - JPG',
+                commands: [
+                  'convert @- -quality 92 scan-%04d.jpg',
+                  'ls scan-*.jpg'
+                ],
+                afterAction: 'Move to Nadine'
+              }
+            ];
           },
-          {
-            name: 'Move to Nadine',
-            async execute(fileInfo) {
-              const options = { paths: ['/usr/lib/scanservjs'] };
-              const Process = require(require.resolve('./server/classes/process', options));
-              return await Process.spawn("mv '" + fileInfo.fullname + "' /var/lib/scanservjs/output/nadine/");
+
+          actions: [
+            {
+              name: 'Move to Lucas',
+              async execute(fileInfo) {
+                const options = { paths: ['/usr/lib/scanservjs'] };
+                const Process = require(require.resolve('./server/classes/process', options));
+                return await Process.spawn("mv '" + fileInfo.fullname + "' /var/lib/scanservjs/output/lucas/");
+              }
+            },
+            {
+              name: 'Move to Nadine',
+              async execute(fileInfo) {
+                const options = { paths: ['/usr/lib/scanservjs'] };
+                const Process = require(require.resolve('./server/classes/process', options));
+                return await Process.spawn("mv '" + fileInfo.fullname + "' /var/lib/scanservjs/output/nadine/");
+              }
             }
-          }
-        ]
-      };
-    '';
+          ]
+        };
+      '';
   };
 
   virtualisation.quadlet.containers.scanservjs.unitConfig."X-RestartTrigger" = [
