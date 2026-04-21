4930e1ce9b69f4ecc5c907d0a1879701ecee4566
Lucas <keyruu@web.de>
2026-04-21 20:12:13 +0200

    fix: paperless redis cap

--- nix/hosts/mentat/modules/stacks/paperless.nix
+++ nix/hosts/mentat/modules/stacks/paperless.nix
@@ -63,6 +63,7 @@ in
             ];
             networkAliases = [ "broker" ];
             addCapabilities = [
+              "DAC_OVERRIDE"
               "SETUID"
               "SETGID"
             ];
