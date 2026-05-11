eb56471a172e3974e2a7b7f0064007950a029f68
Lucas <keyruu@web.de>
2026-05-11 09:16:10 +0200

    feat: remove postgres for koito

--- nix/hosts/prime/modules/stacks/koito.nix
+++ nix/hosts/prime/modules/stacks/koito.nix
@@ -16,18 +16,8 @@ in
       restartUnits = [ (quadlet.service containers.koito-main) ];
       content = ''
         KOITO_SQLITE_ENABLED=true
-        KOITO_DATABASE_URL=postgres://postgres:${config.sops.placeholder.koitoDbPassword}@db:5432/koitodb
-        KOITO_ALLOWED_HOSTS=${my.domain},koito:4110
         KOITO_CORS_ALLOWED_ORIGINS=https://keyruu.de,http://localhost:4321
         KOITO_ENABLE_FULL_IMAGE_CACHE=true
-      '';
-    };
-    "koito-db.env" = {
-      restartUnits = [ (quadlet.service containers.koito-db) ];
-      content = ''
-        POSTGRES_DB=koitodb
-        POSTGRES_USER=postgres
-        POSTGRES_PASSWORD=${config.sops.placeholder.koitoDbPassword}
       '';
     };
   };
@@ -45,30 +35,10 @@ in
       enable = true;
       directories = [
         "data"
-        {
-          path = "db";
-          owner = "999";
-          group = "999";
-        }
       ];
-      network.enable = true;
 
       containers = {
-        db = {
-          containerConfig = {
-            image = "postgres:16";
-            volumes = [ "${my.stack.path}/db:/var/lib/postgresql/data" ];
-            environmentFiles = [ config.sops.templates."koito-db.env".path ];
-            healthCmd = "pg_isready -U postgres -d koitodb";
-            healthInterval = "3s";
-            healthTimeout = "5s";
-            healthRetries = 5;
-            healthStartPeriod = "10s";
-            networkAliases = [ "db" ];
-          };
-        };
-
-        main = {
+        koito = {
           containerConfig =
             # let
             #   inherit (config.virtualisation.quadlet) builds;
@@ -78,9 +48,7 @@ in
               publishPorts = [ "127.0.0.1:${toString my.port}:4110" ];
               volumes = [ "${my.stack.path}/data:/etc/koito" ];
               environmentFiles = [ config.sops.templates."koito-main.env".path ];
-              networkAliases = [ "koito" ];
             };
-          dependsOn = [ "db" ];
         };
       };
     };
