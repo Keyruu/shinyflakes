6430b02397de6962c47df3d387a6886ff9d4bee9
Lucas <keyruu@web.de>
2026-07-28 11:40:17 +0200

    fix: unset is ignored explicitly set

--- nix/hosts/mentat/modules/stacks/media/recyclarr.nix
+++ nix/hosts/mentat/modules/stacks/media/recyclarr.nix
@@ -153,6 +153,7 @@ in
               - name: SQP-1 (1080p-2160p)
                 reset_unmatched_scores:
                   enabled: true
+                min_format_score: 0
                 score_set: sqp-1-2160p
                 upgrade:
                   allowed: true
